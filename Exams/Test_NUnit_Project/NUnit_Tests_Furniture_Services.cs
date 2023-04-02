04f2c93aaf24cd208057a130317e85c6f045bf78
Владимир Михновец <mvikvm2@gmail.com>
2023-04-02 13:21:55 +0300

    Удалил ненужный коментарии в NUnit_Tests_Furniture_Services

--- Exams/Test_NUnit_Project/NUnit_Tests_Furniture_Services.cs
+++ Exams/Test_NUnit_Project/NUnit_Tests_Furniture_Services.cs
@@ -13,10 +13,6 @@ namespace Test_NUnit_Project
     [TestFixture]
     public class NUnit_Tests_Furniture_Services
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
         [Test]
         public void FurnitureServisesInstanceShouldCreateCorrectly()
         {
@@ -30,55 +26,5 @@ namespace Test_NUnit_Project
         {
             FluentActions.Invoking(() => new FurnitureService(null)).Should().Throw<ArgumentNullException>();
         }
-       // [Test]
-        //public void Test1()
-       // {
-            //    //Arrange
-            //    var mock = new Mock<IApplicationContext>();
-            //    mock.Setup()
-            //    //Act
-            //    //Assert
-
-            //    var db = Mock.Of<IApplicationContext>();
-            //    mock.Setup(a => a.GetComputerList()).Returns(new List<Computer>());
-            //    var furniture = Mock.Of<IFurnitureServices>(u => u.GetAllFurniture = list);
-            //    var uowFactory = Mock.Of<IUnitOfWorkFactory>(f => f.Create() == uow);
-
-            //    var service = new SomeService() { UoWFactory = uowFactory };
-
-            //    var result = service.GetAllEntitiesList();
-
-            //    CollectionAssert.AreEqual(list, result);
-            //}
-
-            //private List<PieceOfFurniture> list = new List<PieceOfFurniture> 
-            //    { 
-            //        new PieceOfFurniture() { Name = "Стол", Quantity = 1 },
-            //        new PieceOfFurniture() { Name = "Стул", Quantity = 4 },
-            //        new PieceOfFurniture() { Name = "Шкаф", Quantity = 2 },
-            //        new PieceOfFurniture() { Name = "Диван", Quantity = 1 }
-            //    };
-
-            //    // Arrange
-            //    var mock = new ApplicationContext();
-            //    mock.Setup(a => a.PieceOfFurnitures.GetAllFurniture().Returns(list));
-            //    IFurnitureServices controller = new FurnitureServices(mock);
-
-            //    // Act
-            //    var result = controller.GetAllFurniture()//as IEnumerable<PieceOfFurniture>;
-
-            //    // Assert
-            //    result.Should().NotBeNull();
-            //    result.Should().HaveCount(1);
-            //}
-
-            //private List<PieceOfFurniture> list = new List<PieceOfFurniture>
-            //        {
-            //            new PieceOfFurniture() { Name = "Стол", Quantity = 1 },
-            //            new PieceOfFurniture() { Name = "Стул", Quantity = 4 },
-            //            new PieceOfFurniture() { Name = "Шкаф", Quantity = 2 },
-            //            new PieceOfFurniture() { Name = "Диван", Quantity = 1 }
-            //        };
-        //}
     }
 }
